5647d4f4933685d4032be0d9de1b21ede31f995d
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-05 11:47:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Mints() {
   const mint = async () => {
     setMintBtnStat(true)
     setWalletStatus("Loading...")
-    let cost = mintAmmount * 4000000;
+    let cost = mintAmmount * 3000000000;
     if (contract != null) {
       let idsToMint = random()
       // const preSold = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,47,48,50,51,52,53,54,55,56,57,58,59,60,115,139,140]
