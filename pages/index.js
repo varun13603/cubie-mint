fc4511f8b35af009ef70f2c246c58f3325f3384f
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-06 18:54:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ export default function Mints() {
       </div>
 
       <div className="Banner">
-        <h2>Mint Cubie 𝐌𝐢𝐧𝐭 𝐂𝐮𝐛𝐢𝐞</h2>
+        <h2>𝐌𝐢𝐧𝐭 𝐂𝐮𝐛𝐢𝐞</h2>
         <p><strong>Enter how many Cubies you would like to mint</strong></p>
       </div>
 
