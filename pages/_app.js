7d17d4950f7998cd01c06786d955caeee826179f
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-06 16:40:17 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,13 +13,13 @@ function MyApp({ Component, pageProps }) {
       <div className='Footer'>
         <div className='socials'>
           <a href='https://twitter.com/Cubie3DNFT' target='_blank' rel="noopener noreferrer">
-            <img src='/twitter.png' alt='Twitter' />
+            <img src='/icons8-twitter.gif' alt='Twitter' />
           </a>
           <a href='https://www.cubie.art' target='_blank' rel="noopener noreferrer">
             <img src='/website.png' alt='Website' />
           </a>
           <a href='https://t.me/CubieNFT' target='_blank' rel="noopener noreferrer">
-            <img src='/icons8-twitter.gif' alt='Telegram' />
+            <img src='/' alt='Telegram' />
           </a>
         </div>
         <div className='copyright'>Copyright</div>
