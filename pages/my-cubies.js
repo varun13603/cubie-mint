2f6a7f102e5b58b448a708ca1032151ff62fa718
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-08 10:21:59 +0530

    Update my-cubies.js

--- pages/my-cubies.js
+++ pages/my-cubies.js
@@ -63,7 +63,7 @@ export default function Accounts() {
                 <CardImg src={cubie.image} alt='Cubie Display' />
                 <CardBody>
                   <h5><strong>{cubie.name}</strong></h5>
-                  <p>⛏️Power:  {cubie.power} || {cubie.rarity} </p>
+                  <p><strong>⛏️Power:  {cubie.power} || {cubie.rarity} </strong></p>
                 </CardBody>
               </Card>
             </Col>
