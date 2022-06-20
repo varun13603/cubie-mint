b4ad004e37bee3478d57ddc1f14909d2cf4fed42
Itsme <54501125+varun13603@users.noreply.github.com>
2022-06-20 06:58:31 +0530

    Update 17.js

--- pages/api/cubies/17.js
+++ pages/api/cubies/17.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   res.status(200).json({
     "name"  : "Cubie #17",
     "rarity": "Normal",
-    "power" : "2",
+    "power" : "1",
     "image" : "https://ipfs.filebase.io/ipfs/bafkreicufj2pakdvvk2cmcvhkyz7fgwknn7mqtst3q3mfb5uqztvnrffx4"
   })
 }
